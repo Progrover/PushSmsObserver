bbb9d3a58dc98f78169687815002ae010ff8be97
Progrover <msaharovforwork@gmail.com>
2024-09-15 13:35:18 +0300

    Endpoint removed

--- app/src/main/java/com/bitkor/app/Constants.kt
+++ app/src/main/java/com/bitkor/app/Constants.kt
@@ -1,6 +1,6 @@
 package com.bitkor.app
 
 object Constants {
-    const val ENDPOINT = "https://188.68.204.26/api/message_receiver/"
+    const val ENDPOINT = "********"
     const val VERSION = "2.0.1"
 }
